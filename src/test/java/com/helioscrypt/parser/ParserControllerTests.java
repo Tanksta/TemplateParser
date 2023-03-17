314bc52ee68b5045747c4bd0ada9c055152630fc
Felix Tegelkamp <Tanksta@users.noreply.github.com>
2023-03-17 12:09:09 +0100

    Upate controller to handle xml and json input (#3)

--- src/test/java/com/helioscrypt/parser/ParserControllerTests.java
+++ src/test/java/com/helioscrypt/parser/ParserControllerTests.java
@@ -21,22 +21,45 @@ public class ParserControllerTests {
     private MockMvc mockMvc;
 
     @Test
-    public void multipartShouldReturnJSONObject() throws Exception {
+    public void renderXMLInputShouldReturnJSONObject() throws Exception {
         final String expectedResponseContent = new String(
                 Files.readAllBytes(Paths.get("./fixtures/expected-result.json")));
 
-        String xmlFilePath = "./fixtures/input.xml";
+        String inputFilePath = "./fixtures/input.xml";
         String templateFilePath = "./fixtures/xml2json.liquid";
 
-        byte[] xmlBytes = Files.readAllBytes(Paths.get(xmlFilePath));
+        byte[] inputBytes = Files.readAllBytes(Paths.get(inputFilePath));
         byte[] templateBytes = Files.readAllBytes(Paths.get(templateFilePath));
 
-        MockMultipartFile xmlFile = new MockMultipartFile("xml", "input.xml", null, xmlBytes);
+        MockMultipartFile inputFile = new MockMultipartFile("input", "input.xml", null, inputBytes);
         MockMultipartFile templateFile = new MockMultipartFile("template", "xml2json", null, templateBytes);
 
         this.mockMvc.perform(
-                multipart("/api/v1/parseXML")
-                        .file(xmlFile)
+                multipart("/api/v1/render")
+                        .file(inputFile)
+                        .file(templateFile))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseContent, true));
+    }
+
+    @Test
+    public void renderJSONInputShouldReturnJSONObject() throws Exception {
+        final String expectedResponseContent = new String(
+                Files.readAllBytes(Paths.get("./fixtures/expected-result.json")));
+
+        String inputFilePath = "./fixtures/input.json";
+        String templateFilePath = "./fixtures/xml2json.liquid";
+
+        byte[] inputBytes = Files.readAllBytes(Paths.get(inputFilePath));
+        byte[] templateBytes = Files.readAllBytes(Paths.get(templateFilePath));
+
+        MockMultipartFile inputFile = new MockMultipartFile("input", "input.json", null, inputBytes);
+        MockMultipartFile templateFile = new MockMultipartFile("template", "xml2json", null, templateBytes);
+
+        this.mockMvc.perform(
+                multipart("/api/v1/render")
+                        .file(inputFile)
                         .file(templateFile))
                 .andDo(print())
                 .andExpect(status().isOk())
